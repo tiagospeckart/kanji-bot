37d8c5338cb41b0947dc9d199e59b91eb286b172
tiagospeckart <tiagospeckart@gmail.com>
2023-05-19 23:52:53 -0300

    feat: new Connection to database implementation

--- src/main/java/kanjibot/Bot.java
+++ src/main/java/kanjibot/Bot.java
@@ -1,4 +1,8 @@
 package kanjibot;
+
+import com.mongodb.client.MongoCollection;
+import kanjibot.database.ConnectionDB;
+import org.bson.Document;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -11,6 +15,8 @@ import java.util.Properties;
 public class Bot extends TelegramLongPollingBot {
     private String botUsername;
     private String botToken;
+    private ConnectionDB dbConnection;
+
     public Bot() {
         Properties props = new Properties();
         InputStream is = getClass().getClassLoader().getResourceAsStream("environment.properties");
@@ -19,12 +25,21 @@ public class Bot extends TelegramLongPollingBot {
                 props.load(is);
                 this.botUsername = props.getProperty("BOT_NAME");
                 this.botToken = props.getProperty("BOT_TOKEN");
+
+                this.dbConnection = new ConnectionDB();
+                MongoCollection<Document> collection = dbConnection.getCollection();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else {
             System.out.println("Provide appropriate Bot API Token and Bot");
         }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (dbConnection != null) {
+                dbConnection.closeConnection();
+            }
+        }));
     }
 
     @Override
@@ -39,17 +54,52 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
-            message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(update.getMessage().getText());
+            String messageText = update.getMessage().getText();
 
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            if(messageText.startsWith("/")) {
+                handleCommand(update);
+            } else {
+                handleMessage(update);
             }
+        }
+    }
+
+    private void handleCommand(Update update) {
+        String command = update.getMessage().getText().substring(1); // removes the "/"
+        SendMessage message = new SendMessage();
+
+        // respond according to the command
+        if (command.equals("start")) {
+            message.setText("Welcome to KanjiBot!");
+        } else if (command.equals("help")) {
+            message.setText("Here is a list of commands...");
+        } else {
+            message.setText("Unknown command");
+        }
+
+        message.setChatId(update.getMessage().getChatId().toString());
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleMessage(Update update) {
+        String messageText = update.getMessage().getText();
+        SendMessage message = new SendMessage();
+
+        // ... You can add your logic here ...
+
+        message.setChatId(update.getMessage().getChatId().toString());
+        message.setText("You sent: " + messageText);
+
+        try {
+            execute(message); // Call method to send the message
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/kanjibot/database/ConnectionDB.java
+++ src/main/java/kanjibot/database/ConnectionDB.java
@@ -1,2 +1,78 @@
-package kanjibot.database;public class ConnectionDB {
+package kanjibot.database;
+
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClientSettings;
+import org.bson.Document;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+public class ConnectionDB {
+    private MongoClient mongoClient;
+    private MongoCollection<Document> collection;
+
+    public ConnectionDB() {
+        this.mongoClient = openConnection();
+    }
+
+    private MongoClient openConnection() {
+        Properties props = new Properties();
+        InputStream is = getClass().getClassLoader().getResourceAsStream("environment.properties");
+        if (is != null) {
+            try {
+                props.load(is);
+                String mongoUser = props.getProperty("MONGO_USERNAME");
+                String mongoPassword = props.getProperty("MONGO_PASSWORD");
+                String mongoHost = props.getProperty("MONGO_HOST");
+                String mongoPort = props.getProperty("MONGO_PORT");
+                String mongoDatabaseName = props.getProperty("MONGO_DATABASE");
+                String mongoCollectionName = props.getProperty("MONGO_COLLECTION");
+
+                MongoCredential credential = MongoCredential.createCredential(mongoUser, mongoDatabaseName, mongoPassword.toCharArray());
+
+                MongoClientSettings settings = MongoClientSettings.builder()
+                        .applyToClusterSettings(builder ->
+                                builder.hosts(List.of(new ServerAddress(mongoHost, Integer.parseInt(mongoPort)))))
+                        .credential(credential)
+                        .build();
+
+                mongoClient = MongoClients.create(settings);
+                MongoDatabase database = mongoClient.getDatabase(mongoDatabaseName);
+                collection = database.getCollection(mongoCollectionName);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Failed to load properties file.");
+                return null;
+            }
+        } else {
+            System.out.println("Properties file not found.");
+            return null;
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (mongoClient != null) {
+                mongoClient.close();
+            }
+        }));
+
+        return mongoClient;
+    }
+
+    public MongoCollection<Document> getCollection() {
+        return this.collection;
+    }
+
+    public void closeConnection() {
+        if (mongoClient != null) {
+            mongoClient.close();
+        }
+    }
 }
